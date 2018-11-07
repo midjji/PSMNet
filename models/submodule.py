98908776b89294c106035c7a6504931a18f731cd
Mikael <mikael.p.persson@gmail.com>
2018-11-07 12:47:59 +0100

    stateles

--- models/submodule.py
+++ models/submodule.py
@@ -55,10 +55,13 @@ class matchshifted(nn.Module):
 class disparityregression(nn.Module):
     def __init__(self, maxdisp):
         super(disparityregression, self).__init__()
-        self.disp = Variable(torch.Tensor(np.reshape(np.array(range(maxdisp)),[1,maxdisp,1,1])).cuda(), requires_grad=False)
+        self.maxdisp=maxdisp
 
     def forward(self, x):
-        disp = self.disp.repeat(x.size()[0],1,x.size()[2],x.size()[3])
+        disp = torch.cuda.FloatTensor(np.reshape(np.array(range(self.maxdisp)), [1, self.maxdisp, 1, 1]))
+        disp.requires_grad = False
+        disp = disp.repeat(x.size()[0],1,x.size()[2],x.size()[3])
+        disp.requires_grad = False
         out = torch.sum(x*disp,1)
         return out
 
